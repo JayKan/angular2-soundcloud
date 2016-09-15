3909ae22e28897aaa1044f5ec9b197218bbb6ad5
Jay Kan <leicasper@gmail.com>
2016-09-15 10:40:30 -0700

    chore: add karma config

---
+++ karma.conf.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const argv = require('yargs').argv;
+
+// module.exports = require('./config/karma.conf');
+
+module.exports = config => {
+  const options = {
+    basePath: '',
+
+    frameworks: ['jasmine'],
+
+    files: ['karma.entry.js'],
+
+    preprocessors: {
+      'karma.entry.js': ['coverage', 'webpack', 'sourcemap']
+    },
+
+    webpack: require('./webpack.config'),
+
+    webpackServer: {
+      noInfo: true
+    },
+
+    // reporters: ['dots'],
+    reporters: ['mocha'],
+
+    logLevel: config.LOG_INFO,
+
+    colors: true,
+
+    autoWatch: true,
+
+    singleRun: false,
+
+    browsers: ['Chrome']
+  };
+
+  if (argv.coverage) {
+    options.reporters.push('coverage');
+    options.coverageReporter = {
+      dir: 'coverage/',
+      reporters: [
+        { type: 'text-summary' },
+        { type: 'json' },
+        { type: 'html' }
+      ]
+    };
+  }
+
+  config.set(options);
+};
