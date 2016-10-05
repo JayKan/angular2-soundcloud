acd5ac6719d28271bee7205f51ca7a5cff6b9dcf
Jay Kan <leicasper@gmail.com>
2016-10-05 11:02:47 -0700

    feat: add player and player-control components

---
+++ src/app/player/components/player-controls.ts
@@ -0,0 +1,55 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, EventEmitter, Input, Output } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Track, TracklistCursor } from '../../tracklist';
+import { PlayerState } from '../reducers/player-state';
+
+@Component({
+  selector: 'player-controls',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template:`
+  <div class="player-controls">
+    <div>
+      <icon-button [icon]="'skip-previous'" (onClick)="previous()"></icon-button>
+      <icon-button [icon]="player.isPlaying ? 'pause' : 'play'" (onClick)="player.isPlaying ? pause.emit() : play.emit()"></icon-button>
+      <icon-button [icon]="'skip-next'" (onClick)="next()"></icon-button>
+    </div>
+    
+    <div class="player-controls-time">{{ currentTime | async | formatTime }} / {{ track.duration | formatTime:'ms' }}</div>
+    <div class="player-controls-title">{{ track.title }}</div>
+    
+    <div class="player-controls-volume">
+      <icon-button [icon]="'remove'" (onClick)="decreaseVolume.emit()"></icon-button>
+      <span>{{ player.volume | formatVolume }}</span>
+      <icon-button [icon]="'add'" (onClick)="increaseVolume.emit()"></icon-button>
+    </div>
+  </div>
+  `,
+  styles: [
+    require('./player-controls.scss')
+  ]
+})
+export class PlayerControlsComponent {
+  @Input() currentTime: Observable<number>;
+  @Input() cursor: TracklistCursor;
+  @Input() player: PlayerState;
+  @Input() track: Track;
+
+  @Output() decreaseVolume: EventEmitter<any> = new EventEmitter<any>(false);
+  @Output() increaseVolume: EventEmitter<any> = new EventEmitter<any>(false);
+  @Output() pause: EventEmitter<any> = new EventEmitter<any>(false);
+  @Output() play: EventEmitter<any> = new EventEmitter<any>(false);
+  @Output() select: EventEmitter<any> = new EventEmitter<any>(false);
+
+  next(): void {
+    if (this.cursor.nextTrackId) {
+      this.select.emit(this.cursor.nextTrackId);
+    }
+  }
+
+  previous(): void {
+    if (this.cursor.previousTrackId) {
+      this.select.emit(this.cursor.previousTrackId);
+    }
+  }
+}
---
+++ src/app/player/components/player.ts
@@ -0,0 +1,41 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, ElementRef, Renderer } from '@angular/core';
+import { PlayerService } from '../player-service';
+
+@Component({
+  selector: 'player',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template:`
+  <div class="player-timeline">
+    <audio-timeline
+      [times]="player.times$ | async"
+      (seek)="player.seek($event)">      
+    </audio-timeline>
+  </div>
+  
+  <player-controls
+    [currentTime]="player.currentTime$"
+    [cursor]="player.cursor$ | async"
+    [player]="player.player$ | async"
+    [track]="player.track$ | async"
+    (decreaseVolume)="player.decreaseVolume()"
+    (increaseVolume)="player.increaseVolume()"
+    (pause)="player.pause()"
+    (play)="player.play()"
+    (select)="player.select({trackId: $event})">   
+  </player-controls>   
+  `,
+  styles: [
+    require('./player.scss')
+  ]
+})
+export class PlayerComponent {
+  constructor(public el: ElementRef, public player: PlayerService, public renderer: Renderer) {
+    let sub = player.player$.subscribe(player => {
+      if (player.isPlaying) {
+        renderer.setElementClass(el.nativeElement, 'open', true);
+        sub.unsubscribe();
+      }
+    });
+  }
+}
