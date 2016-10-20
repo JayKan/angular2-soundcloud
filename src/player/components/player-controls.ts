dd5d9e046fb3c90b003969e4f9f1f96229bd7559
Jay Kan <leicasper@gmail.com>
2016-10-20 13:52:56 -0700

    refactor(player-controls): clean up player-controls styles and component

--- src/player/components/player-controls.ts
+++ src/player/components/player-controls.ts
@@ -1,6 +1,6 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy, EventEmitter, Input, Output } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Track, TracklistCursor } from '../../../tracklist';
+import { Track, TracklistCursor } from 'src/tracklists';
 import { PlayerState } from '../reducers/player-state';
 
 @Component({
@@ -9,19 +9,19 @@ import { PlayerState } from '../reducers/player-state';
   changeDetection: ChangeDetectionStrategy.OnPush,
   template:`
   <div class="player-controls">
-    <div>
-      <icon-button [icon]="'skip-previous'" (onClick)="previous()"></icon-button>
-      <icon-button [icon]="player.isPlaying ? 'pause' : 'play'" (onClick)="player.isPlaying ? pause.emit() : play.emit()"></icon-button>
-      <icon-button [icon]="'skip-next'" (onClick)="next()"></icon-button>
+    <div>      
+      <icon-button [icon]="'fa-step-backward'" (onClick)="previous()"></icon-button>
+      <icon-button [icon]="player.isPlaying ? 'fa-pause' : 'fa-play'" (onClick)="player.isPlaying ? pause.emit() : play.emit()"></icon-button>     
+      <icon-button [icon]="'fa-step-forward'" (onClick)="next()"></icon-button>
     </div>
     
     <div class="player-controls-time">{{ currentTime | async | formatTime }} / {{ track?.duration | formatTime:'ms' }}</div>
     <div class="player-controls-title">{{ track?.title }}</div>
     
-    <div class="player-controls-volume">
-      <icon-button [icon]="'remove'" (onClick)="decreaseVolume.emit()"></icon-button>
-      <span>{{ player.volume | formatVolume }}</span>
-      <icon-button [icon]="'add'" (onClick)="increaseVolume.emit()"></icon-button>
+    <div class="player-controls-volume">     
+      <icon-button [icon]="'fa-minus'" (onClick)="decreaseVolume.emit()"></icon-button>
+      <span>{{ player.volume | formatVolume }}</span>     
+      <icon-button [icon]="'fa-plus'" (onClick)="increaseVolume.emit()"></icon-button>
     </div>
   </div>
   `,
