84127e251ec0e9a0d49531ff2961c024a754949f
Jay Kan <leicasper@gmail.com>
2016-10-24 20:21:50 -0700

    chore: add core-js polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,32 +1,19 @@
 // Internet Explorer 9 support
 import 'ie-shim';
 
-// Added parts of es6 which are necessary for your project or your browser support requirements.
-import 'core-js/es6/symbol';
-import 'core-js/es6/object';
-import 'core-js/es6/function';
-import 'core-js/es6/parse-int';
-import 'core-js/es6/parse-float';
-import 'core-js/es6/number';
-import 'core-js/es6/math';
-import 'core-js/es6/string';
-import 'core-js/es6/date';
+// Core-JS
 import 'core-js/es6/array';
-import 'core-js/es6/regexp';
 import 'core-js/es6/map';
 import 'core-js/es6/set';
-import 'core-js/es6/weak-map';
-import 'core-js/es6/weak-set';
-import 'core-js/es6/typed';
-import 'core-js/es6/reflect';
-
+import 'core-js/es6/string';
+import 'core-js/es6/symbol';
 import 'core-js/es7/reflect';
+import 'core-js/fn/array/includes';
+import 'core-js/fn/object/assign';
+
+// Zone
 import 'zone.js/dist/zone';
 
 // Typescript emit helpers polyfill
 import 'ts-helpers';
 
-if (process.env.NODE_ENV === 'development') {
-  Error.stackTraceLimit = Infinity;
-  require('zone.js/dist/long-stack-trace-zone');
-}
