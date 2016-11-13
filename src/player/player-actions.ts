03bb7d3ed864513c5f5daf86c6b2a3ee93f7d6c0
Jay Kan <leicasper@gmail.com>
2016-11-13 10:13:07 -0800

    feat(player): add player actions

--- src/player/player-actions.ts
+++ src/player/player-actions.ts
@@ -4,12 +4,12 @@ import { Times } from './interfaces';
 
 @Injectable()
 export class PlayerActions {
-  static AUDIO_ENDED = 'AUDIO_ENDED';
-  static AUDIO_PAUSED = 'AUDIO_PAUSED';
-  static AUDIO_PLAYING = 'AUDIO_PLAYING';
-  static AUDIO_TIME_UPDATED = 'AUDIO_TIME_UPDATED';
+  static AUDIO_ENDED          = 'AUDIO_ENDED';
+  static AUDIO_PAUSED         = 'AUDIO_PAUSED';
+  static AUDIO_PLAYING        = 'AUDIO_PLAYING';
+  static AUDIO_TIME_UPDATED   = 'AUDIO_TIME_UPDATED';
   static AUDIO_VOLUME_CHANGED = 'AUDIO_VOLUME_CHANGED';
-  static PLAY_SELECTED_TRACK = 'PLAY_SELECTED_TRACK';
+  static PLAY_SELECTED_TRACK  = 'PLAY_SELECTED_TRACK';
 
   audioEnded(): Action {
     return {
