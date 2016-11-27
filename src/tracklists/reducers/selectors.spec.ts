4e956111e08f72a616326a2381408bfc012910a3
Jay Kan <leicasper@gmail.com>
2016-11-27 11:30:30 -0800

    test(selectors): add tracklist selectors spec test

---
+++ src/tracklists/reducers/selectors.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed } from '@angular/core/testing';
+import { Store, StoreModule } from '@ngrx/store';
+import { TRACKS_PER_PAGE } from 'src/constants';
+import { testUtils } from 'src/utils/test';
+import { TracklistRecord } from '../models/tracklist';
+import { TracklistActions } from '../tracklist-actions';
+
+
+describe('tracklists', () => {
+  describe('selectors', () => {
+
+    describe('getCurrentTracklist() selector', () => {});
+
+
+    describe('getTracks() selector', () => {});
+
+    describe('getTracksForCurrentTracklist() selector', () => {
+
+    });
+
+
+  });
+});
