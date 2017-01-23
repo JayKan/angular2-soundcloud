b5052d4f602dfa1be3a370b4779562139c3caf38
Jay Kan <leicasper@gmail.com>
2017-01-23 10:56:11 -0800

    chore: add constants

--- src/constants.ts
+++ src/constants.ts
@@ -13,12 +13,20 @@ export const PAGINATION_PARAMS = `limit=${PAGINATION_LIMIT}&linked_partitioning=
 export const IMAGE_DEFAULT_SIZE = 'large.jpg';
 export const IMAGE_XLARGE_SIZE = 't500x500.jpg';
 
-export const WAVEFORM_IMAGE_HOST = 'w1.sndcdn.com';
-export const WAVEFORM_JSON_HOST = 'wis.sndcdn.com';
-
 export const PLAYER_INITIAL_VOLUME = 10;
 export const PLAYER_MAX_VOLUME = 100;
 export const PLAYER_VOLUME_INCREMENT = 5;
 export const PLAYER_STORAGE_KEY = `${APP_NAME}:player`;
 
+//=====================================
+//  TRACKLISTS
+//-------------------------------------
+export const FEATURED_TRACKLIST_USER_ID = 185676792;
 export const TRACKS_PER_PAGE = 12;
+export const FEATURED_TRACKLIST_ID = 'featured';
+
+//=====================================
+//  WAVEFORMS
+//-------------------------------------
+export const WAVEFORM_IMAGE_HOST = 'w1.sndcdn.com';
+export const WAVEFORM_JSON_HOST = 'wis.sndcdn.com';
