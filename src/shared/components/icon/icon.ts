b0cac8bf323dd9e1b021ac163fb1c1707752827d
Jay Kan <leicasper@gmail.com>
2017-01-23 16:18:27 -0800

    chore: clean icon cmp

--- src/shared/components/icon/icon.ts
+++ src/shared/components/icon/icon.ts
@@ -1,18 +1,18 @@
-import { Component, ViewEncapsulation, Input } from '@angular/core';
+import { Component, ViewEncapsulation, Input, ChangeDetectionStrategy } from '@angular/core';
+import './icon.scss';
 
 @Component({
   selector: 'icon',
   encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   template:`
     <svg [attr.class]="'icon icon--' + name + ' ' + className">
       <use [attr.xlink:href]="'#icon-' + name"></use>
     </svg>
-  `,
-  styles:[
-    require('./icon.scss')
-  ]
+  `
 })
 export class IconComponent {
   @Input() className: string = '';
   @Input() name: string;
+
 }
