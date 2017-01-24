6b8f852e1ffc622d070471e3645f2b02c6757fc3
Jay Kan <leicasper@gmail.com>
2017-01-24 11:03:44 -0800

    chore: refactor audio-source

--- src/player/audio-source.ts
+++ src/player/audio-source.ts
@@ -1,6 +1,11 @@
 export class AudioSource extends HTMLAudioElement {}
 
+export function audioFactory() {
+  return new Audio;
+}
+
 export const AUDIO_SOURCE_PROVIDER = {
   provide: AudioSource,
-  useFactory: () => new Audio()
+  // useFactory: () => new Audio()
+  useFactory: audioFactory
 };
