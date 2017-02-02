f7ddb67853cf57666a80da8e759a4b92604f64fa
Jay Kan <leicasper@gmail.com>
2017-02-02 12:31:09 -0800

    chore: add serach-effects service

--- src/search/search-effects.ts
+++ src/search/search-effects.ts
@@ -9,9 +9,10 @@ import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
-import { AppState } from '..';
+import { AppState } from '../app';
 import { ApiService } from '../core';
-import { getCurrentTracklist, TracklistActions } from '../../tracklist';
+import { getCurrentTracklist, getCurrentTracklistV2 } from 'src/tracklists';
+import { TracklistActions } from 'src/tracklists/tracklist-actions';
 import { SearchActions } from './serach-actions';
 
 @Injectable()
@@ -26,14 +27,16 @@ export class SearchEffects {
   @Effect()
   loadSearchResults = this.actions$
     .ofType(SearchActions.LOAD_SEARCH_RESULTS)
-    .withLatestFrom(this.store$.let(getCurrentTracklist()), (action, tracklist) => ({
+    .withLatestFrom(getCurrentTracklistV2(this.store$), (action, tracklist) => ({
       payload: action.payload,
       tracklist
     }))
     .filter(({ tracklist }) => tracklist.isNew)
     .switchMap(({ payload }) => {
       return this.api.fetchSearchResults(payload.query)
-        .map(data => this.tracklistActions.fetchTracksFulfilled(data, payload.tracklistId))
+        .map(data => {
+          return this.tracklistActions.fetchTracksFulfilled(data, payload.tracklistId)
+        })
         .catch(error => Observable.of(this.tracklistActions.fetchTracksFailed(error)));
     });
 }
