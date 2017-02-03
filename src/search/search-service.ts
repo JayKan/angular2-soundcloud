a00740315b86d6aa86f834b782675974d2fe42db
Jay Kan <leicasper@gmail.com>
2017-02-03 08:43:12 -0800

    chore: add serach service

--- src/search/search-service.ts
+++ src/search/search-service.ts
@@ -3,8 +3,8 @@ import 'rxjs/add/operator/let';
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
-import { AppState } from '../';
-import { getSearchQuery } from './reducers/selectors';
+import { AppState } from '../app';
+import { getSearchQuery, getSearchQueryV2 } from './reducers/selectors';
 import { SearchActions } from './serach-actions';
 
 @Injectable()
@@ -12,11 +12,13 @@ export class SearchService {
   query$: Observable<string>;
 
   constructor(private actions: SearchActions, private store$: Store<AppState>) {
-    this.query$ = store$.let(getSearchQuery());
+    // this.query$ = store$.let(getSearchQuery());
+    this.query$ = getSearchQueryV2(store$);
   }
 
   loadSearchResults(query: string): void {
     if (typeof query === 'string' && query.length) {
+      console.log('Firing loadSearchQuery() action...');
       this.store$.dispatch(
         this.actions.loadSearchResults(query)
       );
