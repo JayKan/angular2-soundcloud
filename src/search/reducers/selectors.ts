49db4f4f9900e81ded7f17b755266e7c088d6fb8
Jay Kan <leicasper@gmail.com>
2017-02-05 13:19:30 -0800

    chore: add search selectors

--- src/search/reducers/selectors.ts
+++ src/search/reducers/selectors.ts
@@ -1,11 +1,29 @@
+import '@ngrx/core/add/operator/select';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/map';
 
-import { AppState } from '../..';
-import { Selector } from '../../core';
+import { compose } from '@ngrx/core/compose';
+import { Observable } from 'rxjs/Observable';
+import { AppState } from 'src/app';
+import { Selector } from 'src/core';
+import { SearchState } from './search-state';
 
+//================================================================
+//  Search Selectors (v1)
+//----------------------------------------------------------------
 export function getSearchQuery(): Selector<AppState, string> {
   return state$ => state$
     .map((state: AppState) => state.search.query)
     .distinctUntilChanged();
 }
+
+//================================================================
+//  Search Selectors (v2)
+//----------------------------------------------------------------
+export function getSearchState(state$: Observable<AppState>): Observable<SearchState> {
+  return state$.select(state => state.search)
+}
+export function getQuery(state$: Observable<SearchState>): Observable<string> {
+  return state$.select(state => state.query)
+}
+export const getSearchQueryV2 = compose(getQuery, getSearchState);
