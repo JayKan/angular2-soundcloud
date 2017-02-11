7ff66b9b34b1cf0245bd2177140a110532753bca
Jay Kan <leicasper@gmail.com>
2017-02-11 12:54:41 -0800

    chore: refactor user-card cmp

--- src/users/components/user-card.ts
+++ src/users/components/user-card.ts
@@ -1,5 +1,6 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy, Input } from '@angular/core';
 import { User } from '../models/user';
+import './user-card.scss';
 
 @Component({
   selector: 'user-card',
@@ -9,40 +10,37 @@ import { User } from '../models/user';
   <article class="user-card">
     <div class="g-row g-cont" *ngIf="user">
       <div class="g-col">
-        <h1 class="user-card__title">{{ user.username }}</h1>
+        <h1 class="user-card-title">{{ user.username }}</h1>
       </div>
     </div>
     
     <div class="g-row g-cont user-stats" *ngIf="user">
       <div class="g-col xs-1/2 sm-1/4">
-        <a class="user-stats__label"
+        <a class="user-stats-label"
           [class.active]="resource === 'tracks'"
           [routerLink]="['/users', user.id, 'tracks']">Tracks</a>
-        <div class="user-stats__value">{{ user.trackCount | formatInteger }}</div>
+        <div class="user-stats-value">{{ user.trackCount | formatInteger }}</div>
       </div>
 
       <div class="g-col xs-1/2 sm-1/4">
-        <a class="user-stats__label"
+        <a class="user-stats-label"
           [class.active]="resource === 'likes'"
           [routerLink]="['/users', user.id, 'likes']">Likes</a>
-        <div class="user-stats__value">{{ user.likesCount | formatInteger }}</div>
+        <div class="user-stats-value">{{ user.likesCount | formatInteger }}</div>
       </div>
 
       <div class="g-col xs-hide sm-1/4">
-        <div class="user-stats__label">Followers</div>
-        <div class="user-stats__value">{{ user.followersCount | formatInteger }}</div>
+        <div class="user-stats-label">Followers</div>
+        <div class="user-stats-value">{{ user.followersCount | formatInteger }}</div>
       </div>
 
       <div class="g-col xs-hide sm-1/4">
-        <div class="user-stats__label">Following</div>
-        <div class="user-stats__value">{{ user.followingsCount | formatInteger }}</div>
+        <div class="user-stats-label">Following</div>
+        <div class="user-stats-value">{{ user.followingsCount | formatInteger }}</div>
       </div>
     </div>   
   </article>
-  `,
-  styles: [
-    require('./user-card.scss')
-  ]
+  `
 })
 export class UserCardComponent {
   @Input() resource: string = 'tracks';
