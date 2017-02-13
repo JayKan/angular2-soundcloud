a3c930284212fc139ee509c5b6a7c1f72c10caed
Jay Kan <leicasper@gmail.com>
2017-02-13 14:19:29 -0800

    feat(users): add users related selectors

--- src/users/reducers/selectors.ts
+++ src/users/reducers/selectors.ts
@@ -1,12 +1,17 @@
+import '@ngrx/core/add/operator/select';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/let';
 import 'rxjs/add/operator/map';
 
-import { AppState } from '../../';
-import { Selector } from '../../core';
+import { Observable } from 'rxjs/Observable';
+import { AppState } from 'src/app';
+import { Selector } from 'src/core';
 import { User } from '../models/user';
 import { UsersState } from './users-reducer';
 
+//================================================================
+//  Users Selectors (v1)
+//----------------------------------------------------------------
 export function getCurrentUser(): Selector<AppState, User> {
   return state$ => state$
     .let(getUsers())
@@ -17,5 +22,17 @@ export function getCurrentUser(): Selector<AppState, User> {
 export function getUsers(): Selector<AppState, UsersState> {
   return state$ => state$
     .map(state => state.users)
+    .distinctUntilChanged();
+}
+
+//================================================================
+//  Users Selectors (v2)
+//----------------------------------------------------------------
+export function getCurrentUsersState(state$: Observable<AppState>): Observable<UsersState> {
+  return state$.select(state => state.users);
+}
+export function getCurrentUserV2(state$: Observable<AppState>): Observable<User> {
+  return state$.let(getCurrentUsersState)
+    .map((users: UsersState) => users.get(users.get('currentUserId')))
     .distinctUntilChanged();
 }
