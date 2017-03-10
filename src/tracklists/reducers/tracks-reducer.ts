34930f24fc8d8aade78c7ecb87c11cfeda308e2e
Jay Kan <leicasper@gmail.com>
2017-03-10 12:41:00 -0800

    chore: add tracks reducer

--- src/tracklists/reducers/tracks-reducer.ts
+++ src/tracklists/reducers/tracks-reducer.ts
@@ -9,13 +9,12 @@ const initialState: TracksState = Map<number, Track>();
 export const tracksReducer: ActionReducer<TracksState> = (state: TracksState = initialState, { type, payload }: Action): TracksState => {
   switch (type) {
     case TracklistActions.FETCH_TRACKS_FULFILLED:
-      let newState = state.withMutations(tracks => {
+      return state.withMutations(tracks => {
         payload.collection.forEach((data: TrackData) => {
           tracks.set(data.id, createTrack(data));
         });
       });
-      // console.log('3. Tracks should be here...: ', newState);
-      return newState;
+
 
     default:
       return state;
